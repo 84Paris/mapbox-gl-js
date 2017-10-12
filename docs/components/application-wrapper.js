fe5bfc752bf0a7b78b5d93d5a0d8faa5d683492f
John Firebaugh <john.firebaugh@gmail.com>
2017-10-12 08:40:45 -0700

    Port docs to batfish

---
+++ docs/components/application-wrapper.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Disable Raven if this isn't a production build, so we don't send development
+// errors to Sentry.
+if (process.env.DEPLOY_ENV !== 'production' && typeof window !== 'undefined') {
+  window.MapboxPageShell.disableRaven();
+}
+
+window.MapboxPageShellProduction = true;
+
+class ApplicationWrapper extends React.Component {
+  render() {
+    return this.props.children;
+  }
+}
+
+export default ApplicationWrapper;
